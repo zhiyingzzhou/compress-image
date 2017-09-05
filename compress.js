fc3e3f1eadf30f639ccc1591cd5e8dbb0e08cb33
zhiyingzzhou <zhiyingzzhou@gmail.com>
2017-09-05 17:33:41 +0800

    fix bug

---
+++ compress.js
@@ -0,0 +1,8 @@
+const compress = require('./index');
+// f1Fp_EJ-Y_cLaOXwhVicYj8HOhIvae3A
+
+compress('./source','./target',{
+    compressPercent: 1,
+    tinifyApiKey: 'f1Fp_EJ-Y_cLaOXwhVicYj8HOhIvae3A',
+    compressLimitCount: 5
+});
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 const path = require("path");
-const { wrap : async } = require("co");
+const { wrap: async } = require("co");
 const tinify = require("tinify");
 const chalk = require("chalk");
 const imageinfo = require('imageinfo');
@@ -12,7 +12,7 @@ const log = console.log;
 const sep = path.sep;
 
 let compressIndex = 0,
-compressCount = 0;
+	compressCount = 0;
 
 // 文件列表
 let fileArr = [];
@@ -24,7 +24,7 @@ let defaultOptions = {
 }
 
 let firstInput,
-isInit = false;
+	isInit = false;
 
 /**
  * @function compressImage
@@ -35,11 +35,11 @@ isInit = false;
  * @param {number} [options[].compressPercent=10] - 压缩比例限制,默认为10
  */
 
-function compressImage(input,output='./.tinify',options=defaultOptions) {
+function compressImage(input, output = './.tinify', options = defaultOptions) {
 	++compressCount;
-	const {compressPercent,tinifyApiKey,compressLimitCount} = options;
-	if(!isInit){
-		if(!tinifyApiKey){
+	const { compressPercent, tinifyApiKey, compressLimitCount } = options;
+	if (!isInit) {
+		if (!tinifyApiKey) {
 			log(chalk.red(
 				`tinifyApiKey is't null or undefined!`
 			));
@@ -49,13 +49,22 @@ function compressImage(input,output='./.tinify',options=defaultOptions) {
 		firstInput = input;
 		// 判断给定的目录是否存在
 		const isInputExists = fs.existsSync(input);
-		if(!isInputExists) process.exit(1);
+		if (!isInputExists) process.exit(1);
 		// 判断output目录是否存在
 		const isOutputExists = fs.existsSync(output);
-		if(!isOutputExists) fs.mkdirSync(output);
+		if (!isOutputExists) fs.mkdirSync(output);
 		// 读取给定目录中的所有图片文件列表
 		fileArr = fs.readdirSync(input);
-		fileArr.forEach((item , index) =>{
+		// 判断给定的目录中是否有文件,如果没有文件直接退出
+		if (fileArr.length === 0) {
+			log();
+			log(
+				chalk.red(`${input} directory has't any file!`)
+			);
+			log();
+			process.exit(1);
+		}
+		fileArr.forEach((item, index) => {
 			fileList[item] = {};
 			fileList[item].index = index;
 			fileList[item].path = item;
@@ -70,11 +79,11 @@ function compressImage(input,output='./.tinify',options=defaultOptions) {
 	 * @returns {Promise}
 	 */
 
-	function tinifyPromise(sourceData){
-		return new Promise( (resolve,reject) => {
-			tinify.fromBuffer(sourceData).toBuffer( (err, resultData) => {
+	function tinifyPromise(sourceData) {
+		return new Promise((resolve, reject) => {
+			tinify.fromBuffer(sourceData).toBuffer((err, resultData) => {
 				if (err) reject(err);
-			    resolve(resultData);
+				resolve(resultData);
 			});
 		});
 	}
@@ -86,9 +95,9 @@ function compressImage(input,output='./.tinify',options=defaultOptions) {
 	 */
 
 	function callBySelf() {
-		compressIndex ++ ;
-		if(compressIndex === fileArr.length) process.exit(1);
-		compressImage(firstInput,output,options);
+		compressIndex++;
+		if (compressIndex === fileArr.length) process.exit(1);
+		compressImage(firstInput, output, options);
 	}
 
 	/**
@@ -99,9 +108,9 @@ function compressImage(input,output='./.tinify',options=defaultOptions) {
 	 * @returns {number} 压缩比例
 	 */
 
-	function caleOptimizePercent(originalSize,compressedSize) {
-		const scale =  (originalSize - compressedSize) / originalSize;
-		return Math.floor(scale*100);
+	function caleOptimizePercent(originalSize, compressedSize) {
+		const scale = (originalSize - compressedSize) / originalSize;
+		return Math.floor(scale * 100);
 	}
 
 	/**
@@ -112,8 +121,8 @@ function compressImage(input,output='./.tinify',options=defaultOptions) {
 	 * @param {string} fileObj[].path - 文件路径
 	 * @returns {void}
 	 */
-	
-	const tinifySync = async(function* ({ index, path:basename }){
+
+	const tinifySync = async(function* ({ index, path: basename }) {
 		log();
 		log(chalk.green(
 			`========== compressing ${basename} start! ==========`
@@ -122,11 +131,11 @@ function compressImage(input,output='./.tinify',options=defaultOptions) {
 			`========== ${basename} 第 ${compressCount} 次压缩! ==========`
 		));
 		// 获取文件名
-		const sourceData = fs.readFileSync(input+sep+basename);
+		const sourceData = fs.readFileSync(input + sep + basename);
 		// 判断文件是否为图片
 		let info = imageinfo(sourceData);
-		const {type} = info;
-		if(!type) {
+		const { type } = info;
+		if (!type) {
 			log(chalk.red(
 				`=================== ${basename} file type is't image format! ===================`
 			));
@@ -135,20 +144,20 @@ function compressImage(input,output='./.tinify',options=defaultOptions) {
 			));
 			log();
 			callBySelf();
-		}else{
-			if(!fileList[basename].size){
-				fileList[basename].size = parseInt(filesize(sourceData.length,0));
+		} else {
+			if (!fileList[basename].size) {
+				fileList[basename].size = parseInt(filesize(sourceData.length, 0));
 			}
 
 			const resultData = yield tinifyPromise(sourceData);
-			
-			let optimizedFileSize = parseInt(filesize(resultData.length,0));
-			const optimizedPercent = caleOptimizePercent(fileList[basename].size,optimizedFileSize);
+
+			let optimizedFileSize = parseInt(filesize(resultData.length, 0));
+			const optimizedPercent = caleOptimizePercent(fileList[basename].size, optimizedFileSize);
 
 			fileList[basename].size = optimizedFileSize;
 
-			let outputFileName = output+sep+basename;
-			const writeResult = fs.writeFileSync(outputFileName,resultData);
+			let outputFileName = output + sep + basename;
+			const writeResult = fs.writeFileSync(outputFileName, resultData);
 
 			log(chalk.green(
 				`========== compress ${basename} success! ==========`
@@ -157,13 +166,13 @@ function compressImage(input,output='./.tinify',options=defaultOptions) {
 				`========== ${basename} compress scale percent: ${optimizedPercent}%`
 			));
 			log();
-			if(optimizedPercent >= compressPercent && compressCount < compressLimitCount) {
-				compressImage(output,output,options);
-			}else{
+			if (optimizedPercent >= compressPercent && compressCount < compressLimitCount) {
+				compressImage(output, output, options);
+			} else {
 				compressCount = 0;
 				callBySelf();
 			}
-			
+
 		}
 	});
 
